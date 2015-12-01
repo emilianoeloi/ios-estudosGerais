ae9872f3d42c734d810f3ef9958c315091704ee3
Emiliano Eloi Silva Barbosa <emilianoeloi@gmail.com>
2015-12-01 07:28:56 -0200

    Initial Commit

---
+++ ios-estudosGeraisTests/ios_estudosGeraisTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_estudosGeraisTests.swift
+//  ios-estudosGeraisTests
+//
+//  Created by Emiliano E. S. Barbosa on 12/1/15.
+//  Copyright © 2015 Emiliano E. S. Barbosa. All rights reserved.
+//
+
+import XCTest
+@testable import ios_estudosGerais
+
+class ios_estudosGeraisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
