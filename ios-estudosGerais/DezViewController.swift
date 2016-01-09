2e6587e0a769cd3f0ede5a38aef01c2449e2c395
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2016-01-09 15:52:44 -0200

    ADD: Make a toDoList App

--- ios-estudosGerais/DezViewController.swift
+++ ios-estudosGerais/DezViewController.swift
@@ -8,6 +8,40 @@
 
 import UIKit
 
-class DezViewController : UIViewController{
+class DezViewController : UIViewController, UITableViewDelegate{
+    
+    @IBOutlet weak var toDoTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        toDoTableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoList[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == UITableViewCellEditingStyle.Delete{
+            toDoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            
+            toDoTableView.reloadData()
+        }
+    }
     
 }
--- ios-estudosGerais/OnzeViewController.swift
+++ ios-estudosGerais/OnzeViewController.swift
@@ -8,6 +8,30 @@
 
 import UIKit
 
-class OnzeViewController : UIViewController{
+var toDoList = [String]()
+
+class OnzeViewController : UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet weak var toDoDescription: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func addItem(sender: AnyObject) {
+        toDoList.append(toDoDescription.text!)
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        toDoDescription.text = ""
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        toDoDescription.resignFirstResponder()
+        return true;
+    }
     
 }
